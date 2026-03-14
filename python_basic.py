adf0032bca6a57cc69b34e8e4909651c5864508d
laxmithapas <shakharthapa2004@gmail.com>
2026-03-14 19:42:23 +0530

    Added info for python basics

---
+++ python_basic.py
@@ -0,0 +1,32 @@
+#Pint & Variables
+
+
+print ("Hello world!")
+
+name = "Alice"
+age = 25
+height = 5.6
+is_student = True
+
+# Data Types
+
+text = "Hello"  # str
+number = 42    # int
+decimal = 3.14   # float
+flag = False    # bool
+
+# User Input
+ 
+name = input("What is your name")
+print("Hello,", name)
+
+#If / Else
+
+age = 18
+
+if age>=18:
+    print("You are an adult")
+elif age >=13:
+    print("You are a teenager")
+else:
+    print("You are a child")
