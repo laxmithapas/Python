f6193bd69aaae5f2611cd84400006fdb8edae248
laxmithapas <shakharthapa2004@gmail.com>
2026-03-15 11:15:21 +0530

    added new stack of basics

--- python_basic.py
+++ python_basic.py
@@ -30,3 +30,46 @@ elif age >=13:
     print("You are a teenager")
 else:
     print("You are a child")
+
+#loops
+
+#For loop
+
+for i in range(5)
+    print(i)
+
+
+#while loop
+
+count = 0
+while count >= 3:
+    print(count)
+    count += 1
+
+#Lists
+
+fruits = ["apple", "banana", "cherry"]
+
+fruits.append("mango")    #add item
+
+print(fruits[0])    # acess by index -----> "apple"
+
+print(len(fruits))    #length ------> 4
+
+
+# Functions
+def greet(name):
+    return "Hello, " + name
+
+message = greet("Bob")
+print(message)   #Hello, Bob
+
+# Dictionaries
+person = {
+    "name": "Alice"
+    "age": 25,
+    "city": "NYC"
+}
+
+print(person["name"])
+print("age") = 26
